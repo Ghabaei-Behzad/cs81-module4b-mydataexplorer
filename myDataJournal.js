548aa757c1a08b8aaf012fc77cc626a74ca52daa
Ghabaei-Behzad <Ghabaei_Behzad@student.smc.edu>
2026-01-15 16:50:47 -0800

    Create myDataJournal.js make an array of objects
    weekData an array of objects consists of day, sleepHours, screenTime, mood, caffeineIntake, and focusLevel. Strings and Numbers.

---
+++ myDataJournal.js
@@ -0,0 +1,23 @@
+//Behzad Ghabaei
+//CS81 - Javascript
+//Instructor Seno
+//Assignment 4B - My Personal Data Objects
+//January 15, 2026
+// myDataJournal.js
+
+//Predictions:
+// 1. I think the most screen time is Sunday as opposed to our example data journal posted which was on Wednesday.
+// 2. My prediction for the best focus would be on Tuesday because of getting enough sleep and caffeine intake.
+// 3. Caffeine impact would be Wednesday because of the most intake, thus higher focus.
+
+//Make an Array of Objects
+
+const weekData = [
+  { day: "Monday", sleepHours: 7, screenTime: 5, mood: "tired", caffeineIntake: 2, focusLevel: 6 },
+  { day: "Tuesday", sleepHours: 8, sreenTime: 4, mood: "productive", caffeineIntake: 1, focusLevel: 9 },
+  { day: "Wednesday", sleepHours: 6, screenTime: 7, mood: "stressed", caffeineIntake: 3, focusLevel: 5 },
+  { day: "Thursday", sleepHours: 7.5, screenTime: 4.5, mood: "productive", caffeineIntake: 1, focusLevel: 8 },
+  { day: "Friday", sleepHours: 6.5, screenTime: 6, mood: "excited", caffeineIntake: 2, focusLevel: 7 },
+  { day: "Saturday", sleepHours: 9, screenTime: 3, mood: "relaxed", caffeineIntake: 0, focusLevel: 4 },
+  { day: "Sunday", sleepHours: 8, screenTime: 8, mood: "lazy", caffeineIntake: 0, focusLevel: 3 }
+  ];
